042eae6544db41f8a8ec6cbb58023fabfb9faebe
yazabara <yazabara@gmail.com>
2015-09-22 11:42:57 +0300

    directive was fixed

--- resources/javascripts/layer-image.js
+++ resources/javascripts/layer-image.js
@@ -480,7 +480,7 @@ imagesApp.directive('layerImage', ['$log', 'SETTINGS', '$document', function ($l
             imgSrc: '@',
             callBackMethod: '&resultFunc'
         },
-        templateUrl: "templates/layer-image-template.html",
+        templateUrl: "pages/templates/layer-image-template.html",
         restrict: 'E',
         link: link
     }
